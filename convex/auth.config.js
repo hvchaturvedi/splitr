d643dbb8e6c914d66e3386e54654e77c3a5d974a
hvchaturvedi <hvchaturvedi1@gmail.com>
2025-08-26 16:41:07 +0530

    Fix folder name from inggest → inngest

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,10 @@
-export default {
+import { authConfig } from "convex/server";
+
+export default authConfig({
   providers: [
     {
       domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
-      applicationID: "convex",
+      applicationID: "convex", // just an identifier
     },
   ],
-};
+});
